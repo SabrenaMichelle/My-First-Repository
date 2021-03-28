7dfb6611c3d42cf6597c4350bebc22c6c76ba80c
SabrenaMichelle <sabrenaray@yahoo.com>
2021-03-28 08:03:04 -0400

    Finished Writing About Me

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let h2 = document.createElement('h2')
-h2.textContent = "This content added by JavaScript"
+let h3 = document.createElement('h3')
+h3.textContent = "About Me"
 
 document.querySelector('body').appendChild(h2)
